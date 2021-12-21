757f9b0dcd9a21d39aca40105da2d638ecaab5f0
galatanw <buisness.galatanw@gmail.com>
2021-12-21 10:20:22 +0200

    create lines

--- canvas.js
+++ canvas.js
@@ -1,39 +1,48 @@
-const canvas = document.querySelector("#canvas");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-canvas.style.background = "black";
-const context = canvas.getContext("2d");
-function createRect(obj, color) {
- context.fillStyle = color;
- return context.fillRect(obj.x, obj.y(), obj.width, obj.height);
-}
-function createArc(obj, color) {
- context.beginPath();
- context.fillStyle = color;
- context.arc(obj.x, obj.y, obj.r, obj.sEng, obj.eEng * Math.PI);
- return context.fill();
-}
-function deleteRect(obj) {
- return context.clearRect(obj.x, obj.y(), obj.width, obj.height);
-}
-function deleteArc(obj) {}
-const box = {
- width: 70,
- height: 70,
- y: function () {
-  return 0;
- },
- x: 50,
-};
-const circle = {
- x: 100,
- y: 50,
- r: 50,
- sEng: 0,
- eEng: 2,
-};
-// 1.
-// create two 50x50 squares anyware in the canvas
+// const canvas = document.querySelector("#canvas");
+// canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.style.background = "purple";
+// const context = canvas.getContext("2d");
+// function createLine(obj,color){
+//     context.beginPath()
+//     context.strokeStyle=color
+//     context.moveTo(obj.sX,obj.sY)
+//     context.lineTo(obj.eX,obj.eY)
+//     context.stroke()
+// }
+
+// function createRect(obj, color) {
+    // context.beginPath()
+//  context.fillStyle = color;
+//  return context.fillRect(obj.x, obj.y(), obj.width, obj.height);
+// }
+// function createArc(obj, color) {
+//  context.beginPath();
+//  context.fillStyle = color;
+//  context.arc(obj.x, obj.y, obj.r, obj.sEng, obj.eEng * Math.PI);
+//  return context.fill();
+// }
+// function deleteRect(obj) {
+//  return context.clearRect(obj.x, obj.y(), obj.width, obj.height);
+// }
+// function deleteArc(obj) {}
+// const box = {
+//  width: 70,
+//  height: 70,
+//  y: function () {
+//   return 0;
+//  },
+//  x: 50,
+// };
+// const circle = {
+//  x: 100,
+//  y: 50,
+//  r: 50,
+//  sEng: 0,
+//  eEng: 2,
+// };
+// // 1.
+// // create two 50x50 squares anyware in the canvas
 // createRect(box, "black");
 // box.x = 100;
 // box.y = function () {
@@ -41,31 +50,31 @@ const circle = {
 // };
 // createRect(box, "black");
 
-// 2.
-// create two 70x70 squares anyware in the canvas
+// // 2.
+// // create two 70x70 squares anyware in the canvas
 // createRect(box, "red");
 // box.x = 100;
 // createRect(box, "red");
 
-// 3.
-// create a blue 30x30 circle in the canvas
+// // 3.
+// // create a blue 30x30 circle in the canvas
 // circle.r = 30;
 // createArc(circle, "blue");
 
-// 4.
-// create a red 50x50 circle in the canvas
+// // 4.
+// // create a red 50x50 circle in the canvas
 // circle.r = 50;
 // createArc(circle, "red");
 
-// 5.
-// create 2 green 50x50 circles anywere in the canvas
+// // 5.
+// // create 2 green 50x50 circles anywere in the canvas
 // circle.r = 50;
 // createArc(circle, "green");
 // circle.x=circle.x+circle.r*2
 // createArc(circle,"green")
 
-// 6.
-// create three boxes in diffrent places that would move fowroad every 20 secondes
+// // 6.
+// // create three boxes in diffrent places that would move fowroad every 20 secondes
 // const first={...box}
 // const second={...box}
 // const third={...box}
@@ -88,53 +97,58 @@ const circle = {
 // }
 // threeBoxesMoveFowroads()
 
-// 7.
-// create a floor object and repesent it as the canvas floor
-// const floor={
-//     width:canvas.width,
-//     height:200,
-//     x:0,
-//         y:function(){
-//         return canvas.height-this.height
+// // 7.
+// // create a floor object and repesent it as the canvas floor
+//     const floor={
+//         width:canvas.width,
+//         height:200,
+//         x:0,
+//             y:function(){
+//             return canvas.height-this.height
+//         }
 //     }
-// }
-// createRect(floor,"yellow")
+//     createRect(floor,"yellow")
 
-// 8.
-// create a player object and display it over the previous question floor object
+// // 8.
+// // create a player object and display it over the previous question floor object
 // const player={
 //     width:canvas.width/10,
 //     height:100,
 //     x:0,
 //         y:function(){
-//         return canvas.height-floor.height-this.height
+//         return floor.y()-this.height
 //     }
 // }
 // createRect(player,"red")
-// // 9.
+// // // 9.
 // // jumpng player
 // let jumpY=0
 // let up=true
 // let jumping=false
+
 // function playerJump(){
+//     const y=floor.y()-player.height
 //     jumping=true
-//     const y=canvas.height-floor.height-player.height
 //     deleteRect(player)
 //     up?jumpY+=1:jumpY-=1
+//     console.log("y ="+player.y(),"-"+jumpY);
 //     player.x+=1
 //     player.y=function(){return y-jumpY}
+
 //     createRect(player,"red")
 //     if(jumpY===10)up=false
 //     if(jumpY===0){jumping=false ;return up=true}
 //     requestAnimationFrame(playerJump)
 // }
+
 // window.addEventListener('keyup',(e)=>{
 //     if(jumping||e.key!="ArrowUp")return
+
 //     playerJump()
 // })
 
-// 10.
-// create 10 squares in random positions
+// // 10.
+// // create 10 squares in random positions
 // for (let index = 0; index < 10; index++) {
 //  const colors = ["red", "purple", "teal", "orange", "blue"];
 //  let y = Math.floor(Math.random() * canvas.height);
@@ -151,8 +165,8 @@ const circle = {
 //  createRect(box, "white");
 // }
 
-// 11.
-// create 10 circles in random positions and sizes
+// // 11.
+// // create 10 circles in random positions and sizes
 // for (let index = 0; index < 10; index++) {
 // circle.r=Math.floor(Math.random() * 100)
 // circle.y = Math.floor(Math.random() * canvas.height);
@@ -164,8 +178,8 @@ const circle = {
 // createArc(circle,"red")
 // }
 
-// 12.
-// create 10 squares in random positions and colors
+// // 12.
+// // create 10 squares in random positions and colors
 // for (let index = 0; index < 10; index++) {
 //  const colors = ["red", "purple", "teal", "orange", "blue"];
 //  let y = Math.floor(Math.random() * canvas.height);
@@ -184,7 +198,7 @@ const circle = {
 //  createRect(box, colors[Math.floor(Math.random() * 5)]);
 // }
 
-// 13.
+// // 13.
 // // draw a smily face using arcs
 // circle.r=100
 // createArc(circle,"white")
@@ -200,4 +214,11 @@ const circle = {
 // circle.eEng=1
 // createArc(circle,"black")
 
+// // create a rectangle with diagonal lines
+// createLine({sX:0,sY:0,eX:200,eY:100},"blue")
+// createLine({sX:200,sY:0,eX:0,eY:100},"blue")
+// createLine({sX:0,sY:0,eX:200,eY:0},"red")
+// createLine({sX:200,sY:0,eX:200,eY:100},"red")
+// createLine({sX:200,sY:100,eX:0,eY:100},"red")
+// createLine({sX:0,sY:100,eX:0,eY:0},"red")
 
